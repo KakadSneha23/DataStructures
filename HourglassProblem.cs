84767dc5dabd5f9e648e0f7963de97b487203780
KakadSneha23 <83602285+KakadSneha23@users.noreply.github.com>
2021-05-03 22:40:47 +0530

    Update HourglassProblem.cs

--- HourglassProblem.cs
+++ HourglassProblem.cs
@@ -1,33 +1,56 @@
-#include <bits/stdc++.h>
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-using namespace std;
+class Solution {
 
-// Complete the hourglassSum function below.
-int hourglassSum(vector<vector<int>> arr) {
-
-
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    vector<vector<int>> arr(6);
-    for (int i = 0; i < 6; i++) {
-        arr[i].resize(6);
-
-        for (int j = 0; j < 6; j++) {
-            cin >> arr[i][j];
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr) {
+        int max=-100;
+        int sum=0;
+        
+        for(int k=0;k<4;k++)
+        {
+            for(int i=0;i<4;i++)
+            {
+                sum=0;
+                for(int j=i;j<i+3;j++)
+                {
+                    sum = sum + arr[k][j];
+                }
+                sum = sum + arr[k+1][i+1];
+                for(int j=i;j<i+3;j++)
+                {
+                    sum = sum + arr[k+2][j];
+                }
+                if(sum>max)
+                    max = sum;
+                //Console.WriteLine("sum : {0} , max : {1}",sum,max);    
+            }
         }
-
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return max;
     }
 
-    int result = hourglassSum(arr);
+    static void Main(string[] args) {
+        int[][] arr = new int[6][];
 
-    fout << result << "\n";
+        for (int i = 0; i < 6; i++) {
+            arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        }
 
-    fout.close();
+        int result = hourglassSum(arr);
 
-    return 0;
+        Console.WriteLine(result);
+
+    }
 }
